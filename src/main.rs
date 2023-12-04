3b2594e6c49c1da72c1a1d11dc9f2ab00b65a90c
SecretPocketCat <jiridexterhanc@gmail.com>
2023-12-04 21:57:02 +0100

    don's set icon to reduce deps

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,8 @@
 
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
-use bevy::window::PrimaryWindow;
-use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
 use bevy_game::GamePlugin; // ToDo: Replace bevy_game with your new crate name.
-use std::io::Cursor;
-use winit::window::Icon;
 
 fn main() {
     let mut app = App::new();
@@ -33,23 +29,5 @@ fn main() {
             ..default()
         }))
         .add_plugins(GamePlugin)
-        .add_systems(Startup, set_window_icon)
         .run();
-}
-
-// Sets the icon on windows and X11
-fn set_window_icon(
-    windows: NonSend<WinitWindows>,
-    primary_window: Query<Entity, With<PrimaryWindow>>,
-) {
-    let primary_entity = primary_window.single();
-    let primary = windows.get_window(primary_entity).unwrap();
-    let icon_buf = Cursor::new(include_bytes!("../build/icon_1024x1024.png"));
-    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
-        let image = image.into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        let icon = Icon::from_rgba(rgba, width, height).unwrap();
-        primary.set_window_icon(Some(icon));
-    };
 }
