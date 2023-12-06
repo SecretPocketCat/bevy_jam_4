ac2d2166de7c6ba23bd617d642c7179c70cbe181
SecretPocketCat <jiridexterhanc@gmail.com>
2023-12-06 09:22:37 +0100

    non grid movement

--- src/player.rs
+++ src/player.rs
@@ -36,63 +36,16 @@ fn handle_input(
     mut commands: Commands,
     map: Res<WorldMap>,
     mut highlighted_hexes: Local<HighlightedHexes>,
-    mut player_q: Query<(&ActionState<GameAction>, &mut AgentCoords), With<Player>>,
+    mut player_q: Query<(&ActionState<GameAction>, &mut Transform), With<Player>>,
+    time: Res<Time>,
 ) {
-    for (input, mut coords) in player_q.iter_mut() {
-        if let Some(dir) = input.axis_pair(GameAction::MoveDir).and_then(|dir| {
-            // just up or down does not make sense with pointy hexes
-            if dir.x() != 0. {
-                Some(dir)
-            } else {
-                None
-            }
-        }) {
-            let angle = Vec2::new(-dir.x(), dir.y()).angle_between(Vec2::Y) + 0.5;
-            let target_hex = coords.neighbor(hexx::Direction::from_pointy_angle(angle));
-
-            if let Some(entity) = map.entities.get(&target_hex) {
-                // Clear highlighted hexes materials
-                for entity in highlighted_hexes
-                    .movement
-                    .iter()
-                    .filter_map(|h| map.entities.get(h))
-                {
-                    commands
-                        .entity(*entity)
-                        .insert(map.default_material.clone());
-                }
-
-                if let Some(selected) = highlighted_hexes.selected {
-                    commands
-                        .entity(map.entities[&selected])
-                        .insert(map.default_material.clone());
-                }
-
-                if input.just_pressed(GameAction::Move) {
-                    coords.0 = target_hex;
-
-                    // movement hexes
-                    highlighted_hexes.movement = coords.ring(1).collect();
-                }
-
-                for (vec, mat) in [(&highlighted_hexes.movement, &map.ring_material)] {
-                    for h in vec {
-                        if let Some(e) = map.entities.get(h) {
-                            commands.entity(*e).insert(mat.clone());
-                        }
-                    }
-                }
-
-                // selected hex
-                commands
-                    .entity(*entity)
-                    .insert(map.selected_material.clone());
-                highlighted_hexes.selected = Some(target_hex);
-            }
-        } else if let Some(selected) = highlighted_hexes.selected.take() {
-            commands
-                .entity(map.entities[&selected])
-                .insert(map.default_material.clone());
-        }
+    for (input, mut t) in player_q.iter_mut() {
+        t.translation += (input
+            .axis_pair(GameAction::MoveDir)
+            .map_or(Vec2::ZERO, |x| x.xy())
+            .normalize_or_zero()
+            * 300.
+            * time.delta_seconds())
+        .extend(0.);
     }
 }
